f0616361120fff4466b8acee39e3dbdf1b3ff6d0
Samuel Soares <samsoares@outlook.com>
2016-09-21 01:16:37 +0000

    Format DxToLineSegments

--- c++/DxfToLineSegments.cpp
+++ c++/DxfToLineSegments.cpp
@@ -17,19 +17,25 @@ struct VPoint {
     double y;
 }; 
 
-void getPoints(string filename); 
+vector<VPoint> getPoints(string filename); 
 string trim(string str); 
 
 int main(int argc, char *argv[])
 {
-    getPoints("../dxf/lowercase_c.dxf"); 
+    vector<VPoint> points = getPoints("../dxf/lowercase_c.dxf"); 
+    
+   // int i = 1; 
+    //for (std::vector<VPoint>::iterator it = points.begin() ; i != points.end(); ++it){
+      //  cout << i++ << ": x=" << *it.x << ", y=" << *it.y << endl; 
+    //}
+    
     return 0; 
 }
 
-void getPoints (string filename){
+vector<VPoint> getPoints (string filename){
     vector<VPoint> points; 
+    int count = 0; 
     
-    VPoint point; 
     ifstream input(filename);
     for (string line; getline(input, line);){
         VPoint point; 
@@ -38,7 +44,7 @@ void getPoints (string filename){
         if (tLine.compare("0") == 1){
             getline(input, line);
             tLine = trim(line); 
-            if (tLine.compare("10") == 1){ // start x
+            if (tLine.compare("10") == 1){ 
                 getline(input, line); 
                 tLine = trim(line); 
                 point.x = stod(line); 
@@ -49,15 +55,39 @@ void getPoints (string filename){
                     getline(input, line); 
                     point.y = stod(trim(line)); 
                     
+                    count++;
                     points.push_back(point); 
-                    cout << "x=" << point.x << ", y=" << point.y << endl; 
+                    cout << count << ": x=" << point.x << ", y=" << point.y << endl; 
+                    
+                    // get second point (endpoint of line segment)
+                    getline(input, line); 
+                    tLine = trim(line);
+                    VPoint point2; 
+                    if (tLine.compare("11") == 1){ 
+                        getline(input, line); 
+                        tLine = trim(line); 
+                        point2.x = stod(line); 
+                        
+                        getline(input, line); 
+                        tLine = trim(line); 
+                        if (tLine.compare("21") == 1){
+                            getline(input, line); 
+                            point2.y = stod(trim(line)); 
+                            
+                            count++;
+                            points.push_back(point2); 
+                           cout << count << ": x=" << point2.x << ", y=" << point2.y << endl; 
+                        }
+                    }
                 }
             }
         }
     }
+    
+    return points; 
 }
 
-std::string trim(std::string str){
+string trim(string str){
     size_t first = str.find_first_not_of(' ');
     size_t last = str.find_last_not_of(' ');
     return str.substr(first, (last-first+1));
